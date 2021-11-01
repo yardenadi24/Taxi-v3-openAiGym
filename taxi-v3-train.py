349ff668fe50509e464f2c016c4bc55147c5e26c
yarden <yardenadi24@gmail.com>
2021-11-01 15:00:29 +0200

    added some vars to train py

--- taxi-v3-train.py
+++ taxi-v3-train.py
@@ -1,3 +1,16 @@
+#imports:
 import gym
 import numpy as np
 import random
+
+#env initialize:
+env = gym.make("Taxi-v3")
+
+#how many action for each state
+actionSize = env.action_space.n
+
+#how many states there is
+stateSize = env.observation_space.np_random
+
+#discount factor:
+discount = 0.95
